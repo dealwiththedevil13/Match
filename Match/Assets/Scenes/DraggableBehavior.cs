3652a5574b9aa688cebbb1d50c8bbb4f9ee3e6f5
Amanda <10932525@uvu.edu>
2022-09-13 11:23:54 -0600

    MatchAnims

--- Match/Assets/Scenes/DraggableBehavior.cs
+++ Match/Assets/Scenes/DraggableBehavior.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DraggableBehavior : MonoBehaviour
@@ -7,7 +6,8 @@ public class DraggableBehavior : MonoBehaviour
     
     private Camera cameraObj;
     public bool draggable;
-    public Vector3 position;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent
 
     void Start()
     {
@@ -16,17 +16,22 @@ public class DraggableBehavior : MonoBehaviour
 
     public IEnumerator OnMouseDown()
     {
+        offset = transform.position -cameraObj.ScreenToWorldPoint(Input.mousePosition);
         draggable = true;
+        startDragEvent.Invoke();
+        yeild return new WaitForFixedUpdate();
 
         while (draggable)
         {
             yeild return new WaitForFixedUpdate(); 
-            position = cameraObj.ScreenToViewportPoint(Input.mousePosition);
+            position = cameraObj.ScreenToViewportPoint(Input.mousePosition) + offset;
+            transform.position = position;
         }
     }
     
     private void OnMouseUp()
     {
         draggable = false;
+        endDragEvent.Invoke();
     }
 }
