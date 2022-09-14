398f50fbf107adddbce744a8d1b800e2fc4f698f
Amanda <10932525@uvu.edu>
2022-09-14 00:10:15 -0600

    Early morning git session.
    edit float Data, IntData and TextLabelBehavior

--- Match/Assets/IntData.cs
+++ Match/Assets/IntData.cs
@@ -6,10 +6,10 @@ public class IntData : ScriptableObject
 {
    public int value;
 
-    public void SetValue(int num)
-    {
-        value = num;
-    }
+   public void SetValue(int num)
+   {
+     value =num;
+   }
 
    public void UpdateValue (int num)
    {
