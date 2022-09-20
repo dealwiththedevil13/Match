b800da821706ee20806a3da72caaa22b5ad3ed50
Amanda <10932525@uvu.edu>
2022-09-19 22:43:05 -0600

    Beginning CoroutineBehavior

---
+++ Match/Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public bool canRun = true;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        Debug.Log("Start");
+        yield return wfsObj;
+        Debug.Log("Late Start");
+
+        while(canRun)
+        {
+            yield return wffuObj;
+            Debug.Log("Run on start");
+        }
+    }
+}
