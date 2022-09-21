f9c59f8571e32fe6e24bf127f26d3b682e7c00a4
Amanda <10932525@uvu.edu>
2022-09-20 21:12:24 -0600

    End of Coroutine section
    prefabs of ring and dot, 321 Go and so on

--- Match/Assets/Scripts/CoroutineBehavior.cs
+++ Match/Assets/Scripts/CoroutineBehavior.cs
@@ -5,28 +5,54 @@ using UnityEngine.Events;
 
 public class CoroutineBehavior : MonoBehaviour
 {
-    public UnityEvent startEvent, repeatEvent, endEvent;
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
     
+    public bool canRun;
     public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
 
-    IEnumerator Start()
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+
+   private IEnumerator Counting()
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
         
-        startEvent.Invoke();
+        startCountEvent.Invoke();
         yield return wfsObj;
 
         while(counterNum.value > 0)
         {
-            repeatEvent.Invoke();
+            repeatCountEvent.Invoke();
             counterNum.value--;
             yield return wfsObj;
 
         }
-        endEvent.Invoke();
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        while(canRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalseEvent.Invoke();
+        }
     }
 }
