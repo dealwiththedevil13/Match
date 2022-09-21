aa921fe5c1e89c8149452061ec3400f29c455ac7
Amanda <10932525@uvu.edu>
2022-09-21 00:04:45 -0600

    Color changing begins

--- Match/Assets/Scripts/SpriteBehavior.cs
+++ Match/Assets/Scripts/SpriteBehavior.cs
@@ -17,4 +17,9 @@ public class SpriteBehavior : MonoBehaviour
     {
         rendererObj.color = obj.value;
     }
+
+    public void ChangeRendererColor(ColorIDDataList obj)
+    {
+        rendererObj.color = obj.currentColor.value;
+    }
 }
